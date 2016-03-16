0ef68b176e466d241361a0e6675d06ac793cf247
Jane Zhang <jane.zhang@hujiang.com>
2016-03-16 21:33:00 +0800

    node type

---
+++ index.js
@@ -0,0 +1,8 @@
+/*
+ * 这些Node属性都是常量,用来存储映射节点对象类型的数值代号
+ * */
+for (var key in Node) {
+  document.write(key, '=' + Node[key]);
+  document.write('<br />');
+  //console.log(key, '=' + Node[key]);
+}
