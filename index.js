e1da568f422eb25fe8ed1bdb63944e2af3a6379b
Jane Zhang <jane.zhang@hujiang.com>
2016-04-06 13:45:52 +0800

    ui events

--- index.js
+++ index.js
@@ -1,8 +1,53 @@
-/*
- * 这些Node属性都是常量,用来存储映射节点对象类型的数值代号
- * */
-for (var key in Node) {
-  document.write(key, '=' + Node[key]);
-  document.write('<br />');
-  //console.log(key, '=' + Node[key]);
-}
+//console.log(DOMStringList())
+//alert(document.implementation.hasFeature('KeyboardEvent','4.0'))
+
+var element = document.getElementById('test');
+
+element.addEventListener('compositionstart', function (e) {
+  console.log('compositionstart');
+  console.log(e);
+  //e.preventDefault();
+  //e.stopPropagation();
+});
+
+element.addEventListener('compositionupdate', function (e) {
+  console.log('compositionupdate');
+  console.log(e);
+  e.preventDefault();
+  //e.stopPropagation();
+});
+
+element.addEventListener('compositionend', function (e) {
+  console.log('compositionend');
+  console.log(e);
+  //e.stopPropagation();
+});
+
+element.addEventListener('keydown', function (e) {
+  console.log('keydown');
+  console.log(e);
+
+  //e.stopPropagation();
+});
+
+element.addEventListener('beforeedit', function (e) {
+  console.log('beforeedit');
+  console.log(e);
+  //e.stopPropagation();
+});
+
+element.addEventListener('edit', function (e) {
+  console.log('edit');
+  console.log(e);
+  //e.stopPropagation();
+});
+
+element.addEventListener('keyup', function (e) {
+  console.log('keyup');
+  console.log(e);
+  //e.stopPropagation();
+});
+
+//document.addEventListener('load',function(){
+//  alert('test')
+//});
